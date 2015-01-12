a0a6252f8b4ef19a7668a619dec66a06d46ac8b8
cao7113 <cao7113@hotmail.com>
2015-01-12 18:27:54 +0800

    add convenient Model.fake method

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,17 +1,19 @@
 Faker::Config.locale = :en #中文支持有问题
 
-# Demo Fg.fake :user
-module Fg #FakeGirl
+#Demo: FakerWithFactoryGirl.fake :user
+module FakerWithFactoryGirl
   extend self
-
-  # FakeGirl.fake :user
-  def seed(model_name, opts = {})
+  def fake(model_name, opts = {})
     opts = {num: 1, strategy: :create}.merge opts
-    #todo check factory?
     1.upto(opts[:num]) do |i|
       FactoryGirl.send(opts[:strategy], model_name, opts[:attrs]||{})
     end
   end
+end
 
-  alias_method :fake, :seed
+#for User model: User.fake
+ActiveSupport.on_load :active_record do
+  def self.fake(opts={})
+    FakerWithFactoryGirl.fake(self.name.underscore, opts)
+  end
 end
